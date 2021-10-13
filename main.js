fce66fb97ff7b675ed124c1fd98297b41661dae0
Didier Domínguez <dadu0699@hotmail.com>
2021-10-12 19:28:05 -0600

    ADD: Functionality

--- main.js
+++ main.js
@@ -1,6 +1,58 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const consoleText = (words, id, colors) => {
+  if (colors === undefined) colors = ['#fff'];
+  let visible = true;
+  let con = document.getElementById('console');
+  let letterCount = 1;
+  let x = 1;
+  let waiting = false;
+  let target = document.getElementById(id)
+  target.setAttribute('style', 'color:' + colors[0])
+
+  window.setInterval(() => {
+
+    if (letterCount === 0 && waiting === false) {
+      waiting = true;
+      target.innerHTML = words[0].substring(0, letterCount)
+      window.setTimeout(() => {
+        let usedColor = colors.shift();
+        colors.push(usedColor);
+        let usedWord = words.shift();
+        words.push(usedWord);
+        x = 1;
+        target.setAttribute('style', 'color:' + colors[0])
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+
+    } else if (letterCount === words[0].length + 1 && waiting === false) {
+      waiting = true;
+      window.setTimeout(() => {
+        x = -1;
+        letterCount += x;
+        waiting = false;
+      }, 1000)
+
+    } else if (waiting === false) {
+      target.innerHTML = words[0].substring(0, letterCount)
+      letterCount += x;
+
+    }
+  }, 120)
+
+  window.setInterval(() => {
+    if (visible === true) {
+      con.className = 'console-underscore hidden'
+      visible = false;
+
+    } else {
+      con.className = 'console-underscore'
+      visible = true;
+
+    }
+  }, 400)
+};
+
+// function([string1, string2], target_id, [color1, color2])    
+consoleText(['Hello World.', 'Console Text', 'Made with Love.'], 'text', ['tomato', 'rebeccapurple', 'lightblue']);
